01688fdc95a6bc20a82e7f4fda1d5ad9e913b736
sh95fit <h95fit@gmail.com>
2024-12-04 17:25:56 +0900

    WebView를 이용한 안드로이드 환경 전체 화면 구현

--- FullScreenApp/app/src/main/java/com/example/fullscreenapp/MainActivity.kt
+++ FullScreenApp/app/src/main/java/com/example/fullscreenapp/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.fullscreenapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // WebView 초기화
+        val webView: WebView = findViewById(R.id.webview)
+
+        // WebView 디버깅 활성화 (크롬에서 디버깅 가능)
+        WebView.setWebContentsDebuggingEnabled(true)
+
+        // WebView 설정
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true // JavaScript 활성화
+        webSettings.domStorageEnabled = true // 로컬 스토리지 활성화
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        // WebViewClient 설정 (새로운 창에서 링크를 여는 것을 방지)
+        webView.webViewClient = WebViewClient()
+
+        // React 웹 앱 URL 로드 (배포된 URL)
+        webView.loadUrl("https://monitoring.itsmore.co.kr/ikea")
+
+        // 상태 바와 내비게이션 바 숨기기
+        hideSystemUI()
     }
+
+    // 상태 바와 내비게이션 바 숨기기 함수
+    private fun hideSystemUI() {
+        // 전체 화면 모드로 설정 (상태 바와 내비게이션 바 숨김)
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+        actionBar?.hide()  // 액션 바 숨기기
+    }
+
 }
